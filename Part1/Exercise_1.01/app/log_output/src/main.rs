1624a4139b8c63ae44cfd52fdcf68b8fe5bc8155
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-12-10 18:09:01 +0000

    Update app for Exercise 1.01

--- Part1/Exercise_1.01/app/log_output/src/main.rs
+++ Part1/Exercise_1.01/app/log_output/src/main.rs
@@ -7,16 +7,15 @@ const STRING_LENGTH: usize = 37;
 const CHARSET: &str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 fn main() {
-    let mut i: i32 = 1;
     let timeout = time::Duration::from_millis(TIMEOUT_MS);
     let s: String = String::from(generate(STRING_LENGTH, CHARSET));
 
-    while i < 10 {
+    loop {
         let now = time::SystemTime::now();
         let now: DateTime<Utc> = now.into();
 
         println!("{}: {}",now, s);
-        i += 1;
         thread::sleep(timeout);
     }
 }
+
